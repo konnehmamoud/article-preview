a0db91ec5ff949144a6c90bae1ae96cf93febe5d
konnehmamoud <konnehmamoud@gmail.com>
2024-06-17 12:15:22 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// document.getElementById('shareButton').addEventListener('click', function() {
+//     const sharePopup = document.getElementById('sharePopup');
+//     sharePopup.classList.toggle('active');
+// });
+
+// // Close the share popup when clicking outside
+// document.addEventListener('click', function(event) {
+//     const sharePopup = document.getElementById('sharePopup');
+//     const shareButton = document.getElementById('shareButton');
+//     if (!shareButton.contains(event.target) && !sharePopup.contains(event.target)) {
+//         sharePopup.classList.remove('active');
+//     }
+// });
