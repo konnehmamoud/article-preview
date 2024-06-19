84af8cdd2cf199a3f5181504d4f7e70660ac1bf1
konnehmamoud <konnehmamoud@gmail.com>
2024-06-19 17:38:53 -0500

    fixed code

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-// document.getElementById('shareButton').addEventListener('click', function() {
-//     const sharePopup = document.getElementById('sharePopup');
-//     sharePopup.classList.toggle('active');
-// });
+const shareButton = document.getElementById('shareButton');
+const sharePopup = document.getElementById('sharePopup');
 
-// // Close the share popup when clicking outside
-// document.addEventListener('click', function(event) {
-//     const sharePopup = document.getElementById('sharePopup');
-//     const shareButton = document.getElementById('shareButton');
-//     if (!shareButton.contains(event.target) && !sharePopup.contains(event.target)) {
-//         sharePopup.classList.remove('active');
-//     }
-// });
+shareButton.onclick = function() {
+  sharePopup.classList.toggle('active');
+  if (sharePopup.style.display === 'flex') {
+    sharePopup.style.display = 'none';
+  } else {
+    sharePopup.style.display = 'flex';
+  }
+};
+
